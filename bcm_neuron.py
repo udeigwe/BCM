ce3f612c9ed03666e12621b75b8085b40144b92c
udeigwe <lawrence.udeigwe@manhattan.edu>
2022-04-11 00:05:26 -0400

    first commit

--- bcm_neuron.py
+++ bcm_neuron.py
@@ -11,11 +11,11 @@ from scipy.integrate import quad
 
 
 class BCMNeuron:
-    def __init__(self, lgn_l, lgn_r, p=2, eta=0.005, tau=1000, c0=50, condition='NR'):
+    def __init__(self, lgn_l, lgn_r, patterns, p=2, eta=0.005, tau=1000, c0=50, condition='NR'):
         self.lgn_l = lgn_l  # Number of LGN-cortical input fibers from left eye
         self.lgn_r = lgn_r  # Number of LGN-cortical input fibers from right eye
-        self.np = np  # Number of training patterns
-        self.p = p  # Nonlinearity in the definition of theta, here, theta is u
+        self.patterns = patterns  # Number of training patterns
+        self.p = p  # Nonlinearity in the definition of theta
         self.eta = eta  # Modification step size
         self.tau = tau  # Time constant in the definition of theta
         self.c0 = c0  # Normalization constant in the definition of theta
@@ -27,7 +27,13 @@ class BCMNeuron:
         #       BD = binocular deprivation
         self.ml = 0.15 * np.random.rand(1, self.lgn_l)  # weights initialized to numbers btw 0 and 0.15
         self.mr = 0.15 * np.random.rand(1, self.lgn_r)  # weights initialized to numbers btw 0 and 0.15
-        self.theta = np.random.rand()
+        self.weights_l = np.zeros((self.lgn_l,))  # record matrix; each row is a weight vector for the iteration
+        self.weights_r = np.zeros((self.lgn_r,))  # record matrix; each row is a weight vector for the iteration
+        self.theta = 0.15 * np.random.rand()  # sliding threshold initialized to a number btw 0 and 0.15
+        self.thresholds = self.theta # record array of thresholds
+        self.tuning_curve_l = np.zeros((self.patterns,))  # record matrix; each row is a tuning curve for the iteration
+        self.tuning_curve_r = np.zeros((self.patterns,))  # record matrix; each row is a tuning curve for the iteration
+
 
     def train(self, da_l, da_r, ds_l, ds_r, nl=0, nr=0, iterations=1000):
         # da = actual pattern; ds = spontaneous; n = non-lgn noise
@@ -35,8 +41,6 @@ class BCMNeuron:
         ca = sum(self.ml * da_l[0]) + sum(self.mr * da_r[0])
         cs = sum(self.ml * ds_l[0]) + sum(self.mr * ds_r[0])
         cn = sum(self.ml * nl[0]) + sum(self.mr * nr[0])
-        CL = np.zeros((self.lgn_l,))
-        CR = np.zeros((self.lgn_r,))
 
         if (self.lgn_l != np.shape(da_l)[1]) or (self.lgn_r != np.shape(da_r)[1]):
             raise Exception('pattern size must equal weight (i.e lgn) size ')
@@ -53,17 +57,42 @@ class BCMNeuron:
                 phi = c * (c - self.theta)
                 self.ml = self.ml + self.eta * phi * (da_l + ds_l + nl)  # update left weights
                 self.mr = self.mr + self.eta * phi * (da_r + ds_r + nr)  # update right weights
-                #       We need to now update theta
+                # Add new weights to the weight matrices
+                self.weights_l = np.vstack((self.weights_l,self.ml))
+                self.weights_r = np.vstack((self.weights_r,self.mr))
+                # We need to now update theta
                 ca_bar = (1 / self.tau) * quad(lambda s: ca * np.exp(-(t - s) / self.tau), -np.inf, t)
                 self.theta = (ca_bar ** self.p) / self.c0
-                #       Add responses to all orientation in the big matrix
-                CL = np.vstack((CL, np.sum((da_l + ds_l + nl)*self.ml, axis=1)))
-                CR = np.vstack((CL, np.sum((da_r + ds_r + nr) * self.mr, axis=1)))
+                # Add new theta to the vector of thresholds
+                self.thresholds = np.hstack((self.threshold, self.theta))
+                # Add responses to all orientation in the tuning curve matrix
+                self.tuning_curve_l = np.vstack((self.tuning_curve_l, np.sum((da_l + ds_l + nl)*self.ml, axis=1)))
+                self.tuning_curve_r = np.vstack((self.tuning_curve_r, np.sum((da_r + ds_r + nr) * self.mr, axis=1)))
+        # Now delete the first entries (initializations) of the record matrices/vectors
+        self.weights_l = np.delete(self.weights_l, 0, axis=0)
+        self.weights_r = np.delete(self.weights_r, 0, axis=0)
+        self.thresholds = np.delete(self.threshold, 0)
+        self.tuning_curve_l = np.delete(self.tuning_curve_l, 0, axis=0)
+        self.tuning_curve_r = np.delete(self.tuning_curve_r, 0, axis=0)
 
-        CL = np.delete(CL, 0, axis=0)
-        CR = np.delete(CR, 0, axis=0)
+        return self.weights_l, self.weights_r, self.thresholds, self.tuning_curve_l, self.tuning_curve_r
 
-        return CL, CR,
+    def train_nr(self):
+
+    def train_md(self):
+
+    def train_rs(self):
+
+    def train_bd(self):
+
+    def train_re(self):
+
+    def train_st(self):
+
+    def get_response(self):
+    def plot_weights(self):
 
 
-        # write code to return weights too, theta
+    def plot_response(self,plot_theta=True):
+
+    def plot_tuning_curve (self):
