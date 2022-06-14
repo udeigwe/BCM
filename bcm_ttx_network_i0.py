eb28d32eca39c224513e3f0a723fa9c3d65d3561
Lawrence Udeigwe <udeigwe@braintree-cpu-1.mit.edu>
2022-06-14 16:23:29 -0400

    commit 06/14/22

--- bcm_ttx_network_i0.py
+++ bcm_ttx_network_i0.py
@@ -1,4 +1,4 @@
-# TTX injected in the ipsilateral eye (here, right eye);
+# TTX injected in the ipsilateral eye (here, right eye); 
 # TTX reduces inhibition to neuron coming from an inhibitory neuron
 
 
@@ -13,24 +13,25 @@ from torch.optim.lr_scheduler import StepLR
 import numpy as np
 from scipy.integrate import quad
 import matplotlib.pyplot as plt
-# hashtag hashtaggers
 
 class BCMNeuron:
     def __init__(self, lgn_l, lgn_r, patterns, p=2, tau_w=10, tau_theta=1, dt=0.01, eps=0,
-                 c0=50, condition='NR', iterations=60000, ttx_point=0.50, tau_ttx=300,
-                 we=1, wi=0.09):
+                 c0=50, condition='NR', iterations=80000, ttx_in = 0.40, ttx_gone =0.30, tau_ttx=400,
+                 contra_factor =1.5, we=1, wi=0.1):
         self.lgn_l = lgn_l  # Number of LGN-cortical input fibers from left eye
         self.lgn_r = lgn_r  # Number of LGN-cortical input fibers from right eye
         self.patterns = patterns  # Number of training patterns
         self.p = p  # Nonlinearity in the definition of theta
         self.tau_w = tau_w  # Modification step size
         self.tau_theta = tau_theta  # Time constant in the definition of theta
-        self.dt = dt  # simulation mesh size
-        self.eps = eps;  # regularization constant for weights
+        self.dt = dt # simulation mesh size
+        self.eps = eps; #regularization constant for weights 
         self.c0 = c0  # Normalization constant in the definition of theta
         self.iterations = iterations
-        self.ttx_in = np.floor(ttx_point * iterations)  # time of ttx injection
+        self.ttx_in = np.floor(ttx_in*iterations) # time of ttx injection
+        self.ttx_gone = np.floor(ttx_gone*iterations) # time of ttx starts to wear out
         self.tau_ttx = tau_ttx
+        self.contra_factor = contra_factor
         self.condition = condition
         #       NR = normal rearing,
         #       MD = monocular deprivation,
@@ -39,12 +40,12 @@ class BCMNeuron:
         #       BD = binocular deprivation
         #       TTX_decay = TTX injected in the ipsilatral eye (here, right eye); TTX decays
         #       TTX_inh = TTX injected in the ipsilateral eye (here, right eye); TTX reduces inhibition to neuron
-
-        ### variables for excitatory (present) BCM neuron
+        
+        ### variables for excitatory (present) BCM neuron       
         self.ml = 0.15 * np.random.rand(1, self.lgn_l)  # weights initialized to numbers btw 0 and 0.15
         self.mr = 0.15 * np.random.rand(1, self.lgn_r)  # weights initialized to numbers btw 0 and 0.15
-        self.weights_l = np.random.rand(1, self.lgn_l)  # record matrix; each row is a weight vector for the iteration
-        self.weights_r = np.random.rand(1, self.lgn_r, )  # record matrix; each row is a weight vector for the iteration
+        self.weights_l = np.random.rand(1,self.lgn_l)  # record matrix; each row is a weight vector for the iteration
+        self.weights_r = np.random.rand(1,self.lgn_r,)  # record matrix; each row is a weight vector for the iteration
         self.theta = 0.15 * np.random.rand()  # sliding threshold initialized to a number btw 0 and 0.15
         self.thresholds = self.theta  # record array of thresholds
         self.tuning_curve_l = np.zeros((self.patterns,))  # record matrix; each row is a tuning curve for the iteration
@@ -53,132 +54,139 @@ class BCMNeuron:
         ### variable for inhibitory neuron
         self.mli = 0.15 * np.random.rand(1, self.lgn_l)  # weights initialized to numbers btw 0 and 0.15
         self.mri = 0.15 * np.random.rand(1, self.lgn_r)  # weights initialized to numbers btw 0 and 0.15
-        self.weights_li = np.random.rand(1, self.lgn_l)  # record matrix; each row is a weight vector for the iteration
-        self.weights_ri = np.random.rand(1,
-                                         self.lgn_r, )  # record matrix; each row is a weight vector for the iteration
+        self.weights_li = np.random.rand(1,self.lgn_l)  # record matrix; each row is a weight vector for the iteration
+        self.weights_ri = np.random.rand(1,self.lgn_r,)  # record matrix; each row is a weight vector for the iteration
         self.thetai = 0.15 * np.random.rand()  # sliding threshold initialized to a number btw 0 and 0.15
         self.thresholdsi = self.thetai  # record array of thresholds
         self.tuning_curve_li = np.zeros((self.patterns,))  # record matrix; each row is a tuning curve for the iteration
         self.tuning_curve_ri = np.zeros((self.patterns,))  # record matrix; each row is a tuning curve for the iteration
 
         ### inhibition parameters
-        self.we = we  # excitatory weight from E to I
-        self.wi = wi  # Inhibitory weight from I to E
+        self.we = we # excitatory weight from E to I
+        self.wi = wi # Inhibitory weight from I to E
 
-    def train(self, da_l, da_r, da_li, da_ri, ds_l=0, ds_r=0,
+    def train(self, da_l, da_r, da_li, da_ri, ds_l=0, ds_r=0, 
               ds_li=0, ds_ri=0):
         # da => actual pattern; ds => spontaneous; n => non-lgn noise
         # r => right, l => left, i => inhibitory
-
-        a = 0.00001  # noise scaler
+        
+        al = 0.1 # noise scaler
+        ar = 0.00001 # noise scaler
+        bl = 0.00001 # noise scaler
+        br = 0.00001 # noise scaler
         # ttx_in = np.floor(0.5*self.iterations)  # time of ttx injection
         # ttx_gone =  np.floor(0.75*self.iterations) # time when ttx wears out
         # ttx_decay = 10
-
+      
         if (self.lgn_l != np.shape(da_l)[1]) or (self.lgn_r != np.shape(da_r)[1]):
             raise Exception('pattern size must equal weight (i.e lgn) size ')
         elif (self.lgn_l != np.shape(ds_l)[1]) or (self.lgn_r != np.shape(ds_r)[1]):
             raise Exception('pattern size must equal weight (i.e lgn) size ')
         else:
-
+            
             for i in range(self.iterations):
-
+                print(i)
                 t = i * self.dt
                 k = np.random.randint(np.shape(da_l)[0])
-
+                
                 # generate noise
-                nl = a * np.random.rand(self.patterns, self.lgn_l)
-                nr = a * np.random.rand(self.patterns, self.lgn_r)
-                nli = a * np.random.rand(self.patterns, self.lgn_l)
-                nri = a * np.random.rand(self.patterns, self.lgn_r)
-
+                nl = al*np.random.rand(self.patterns,self.lgn_l)
+                nr = ar*np.random.rand(self.patterns,self.lgn_r)
+                nli = al*np.random.rand(self.patterns,self.lgn_l)
+                nri = ar*np.random.rand(self.patterns,self.lgn_r)
+                
+                
                 # compute BCM neuron activity
                 # ca = np.sum(self.ml * da_l[k]) + np.sum(self.mr * da_r[k])
                 # cs = np.sum(self.ml * ds_l[k]) + np.sum(self.mr * ds_r[k])
                 # cn = np.sum(self.ml * nl[k]) + np.sum(self.mr * nr[k])
-                # c = ca - cs + cn
-
+                # c = ca - cs + cn 
+              
                 # compute inhibitory neuron activity
                 cai = np.sum(self.mli * da_li[k]) + np.sum(self.mri * da_ri[k])
                 csi = np.sum(self.mli * ds_li[k]) + np.sum(self.mri * ds_ri[k])
                 cni = np.sum(self.mli * nli[k]) + np.sum(self.mri * nri[k])
                 ci = cai - csi + cni
-
+                
+                
                 # nonlinear funtion of threshold and activity for inhibitory neuron
-                phii = ci * (ci - self.thetai)
-                ci = 0
-
-                if i <= self.ttx_in:
-                    # compute BCM neuron activity
-                    ca = 0 * np.sum(self.ml * da_l[k]) + np.sum(self.mr * da_r[k])  # only right eye gets input
-                    cs = 0 * np.sum(self.ml * ds_l[k]) + np.sum(self.mr * ds_r[k])  # only right eye gets input
-                    cn = np.sum(self.ml * nl[k]) + np.sum(self.mr * nr[k])  # both eyes get noise
-                    c = ca - cs + cn
-                    # Feedback loop
-                    # ci = ci + self.we*c
-                    # c = c - self.wi*ci
-                    # nonlinear function of threshold and activity
-                    phi = c * (c - self.theta)
-                    # update left weights for BCM neuron
-                    self.ml = self.ml + (1 / self.tau_w) * self.dt * (
-                                phi * (0 * da_l[k] - 0 * ds_l[k] + nl[k]) - self.eps * self.ml)
-                    # update right weights for BCM neuron
-                    self.mr = self.mr + (1 / self.tau_w) * self.dt * (
-                                phi * (da_r[k] - ds_r[k] + nr[k]) - self.eps * self.mr)
-                    # Add responses to all orientation in the tuning curve matrix
-                    self.tuning_curve_l = np.vstack(
-                        (self.tuning_curve_l, np.sum((0 * da_l - 0 * ds_l + nl) * self.ml, axis=1)))
-                    self.tuning_curve_r = np.vstack((self.tuning_curve_r, np.sum((da_r - ds_r + nr) * self.mr, axis=1)))
+                phii = ci*(ci - self.thetai)
+                ci=0
+              
+                if i <= self.ttx_in:  
+                   # generate noise
+                   nl = al*np.random.rand(self.patterns,self.lgn_l)
+                   nr = ar*np.random.rand(self.patterns,self.lgn_r)
+                   nli = al*np.random.rand(self.patterns,self.lgn_l)
+                   nri = ar*np.random.rand(self.patterns,self.lgn_r)
+                   # compute BCM neuron activity
+                   ca = 0*np.sum(self.ml * da_l[k]) + np.sum(self.mr * da_r[k])  # only right eye gets input
+                   cs = 0*np.sum(self.ml * ds_l[k]) + np.sum(self.mr * ds_r[k])  # only right eye gets input
+                   cn = self.contra_factor*np.sum(self.ml * nl[k]) + np.sum(self.mr * nr[k])   # both eyes get noise
+                   c = ca - cs + cn 
+                   # Feedback loop
+                   ci = ci + self.we*c
+                   c = c - self.wi*ci
+                   # nonlinear funtion of threshold and activity
+                   phi = c*(c - self.theta)
+                   # update left weights for BCM neuron
+                   self.ml = self.ml + (1/self.tau_w) * self.dt * (phi * (0*da_l[k] - 0*ds_l[k] + nl[k]) - self.eps*self.ml)  
+                   # update right weights for BCM neuron      
+                   self.mr = self.mr + (1/self.tau_w) * self.dt * (phi * (da_r[k] - ds_r[k] + nr[k]) - self.eps*self.mr) 
+                   # Add responses to all orientation in the tuning curve matrix
+                   self.tuning_curve_l = np.vstack((self.tuning_curve_l, self.contra_factor * np.sum((da_l - ds_l + nl) * self.ml, axis=1)))
+                   self.tuning_curve_r = np.vstack((self.tuning_curve_r, np.sum((da_r - ds_r + nr) * self.mr, axis=1)))
                 else:
-                    # ttx_factor = np.exp((i-ttx_gone)*np.heaviside(ttx_gone-i,0))
-                    ttx_factor = (1 - np.exp(self.dt * (1 / self.tau_ttx) * (self.ttx_in - i)))
-                    # ttx_factor = np.heaviside(i-60000,0)
-                    # ttx_factor = 0
-                    # print(ttx_factor)
-                    # compute BCM neuron activity
-                    ca = np.sum(self.ml * da_l[k]) + ttx_factor * np.sum(self.mr * da_r[k])
-                    cs = np.sum(self.ml * ds_l[k]) + ttx_factor * np.sum(self.mr * ds_r[k])
-                    cn = np.sum(self.ml * nl[k]) + ttx_factor * np.sum(self.mr * nr[k])
-                    c = ca - cs + cn
-                    # Feedback loop
-                    ci = ci + self.we * c
-                    c = c - self.wi * ci
-                    # nonlinear funtion of threshold and activity
-                    phi = c * (c - self.theta)
-                    # update left weights for BCM neuron
-                    self.ml = self.ml + (1 / self.tau_w) * self.dt * (
-                                phi * (da_l[k] - ds_l[k] + nl[k]) - self.eps * self.ml)
-                    # update right weights for BCM neuron
-                    self.mr = self.mr + (1 / self.tau_w) * self.dt * (
-                                phi * ttx_factor * (da_r[k] - ds_r[k] + nr[k]) - self.eps * self.mr)
-                    # Add responses to all orientation in the tuning curve matrix
-                    self.tuning_curve_l = np.vstack((self.tuning_curve_l, np.sum((da_l - ds_l + nl) * self.ml, axis=1)))
-                    self.tuning_curve_r = np.vstack(
-                        (self.tuning_curve_r, np.sum(ttx_factor * (da_r - ds_r + nr) * self.mr, axis=1)))
-
+                   # generate noise
+                   nl = bl*np.random.rand(self.patterns,self.lgn_l)
+                   nr = br*np.random.rand(self.patterns,self.lgn_r)
+                   nli = bl*np.random.rand(self.patterns,self.lgn_l)
+                   nri = br*np.random.rand(self.patterns,self.lgn_r)
+                   # ttx_factor = np.exp((i-ttx_gone)*np.heaviside(ttx_gone-i,0))
+                   #ttx_factor = (1- np.exp(self.dt*(1/self.tau_ttx)*(self.ttx_in-i)))
+                   ttx_factor = np.heaviside(i-self.ttx_gone,0)*\
+                                (1- np.exp(self.dt*(1/self.tau_ttx)*(self.ttx_gone-i)))
+                   # ttx_factor = np.heaviside(i-60000,0)
+                   # ttx_factor = 0
+                   # print(ttx_factor)
+                   # compute BCM neuron activity
+                   ca =  np.sum(self.ml * da_l[k]) + ttx_factor * np.sum(self.mr * da_r[k])
+                   cs =  np.sum(self.ml * ds_l[k]) + ttx_factor * np.sum(self.mr * ds_r[k])
+                   cn =  np.sum(self.ml * nl[k]) + ttx_factor * np.sum(self.mr * nr[k])
+                   c = ca - cs + cn 
+                   # Feedback loop
+                   ci = ci + self.we*c
+                   c = c - self.wi*ci
+                   # nonlinear funtion of threshold and activity
+                   phi = c*(c - self.theta)
+                   # update left weights for BCM neuron
+                   self.ml = self.ml + (1/self.tau_w) * self.dt * (phi * (da_l[k] - ds_l[k] + nl[k]) - self.eps*self.ml)  
+                   # update right weights for BCM neuron   
+                   self.mr = self.mr + (1/self.tau_w) * self.dt * (phi * ttx_factor * (da_r[k] - ds_r[k] + nr[k]) - self.eps*self.mr) 
+                   # Add responses to all orientation in the tuning curve matrix
+                   self.tuning_curve_l = np.vstack((self.tuning_curve_l, np.sum(self.contra_factor* (da_l - ds_l + nl) * self.ml, axis=1)))
+                   # self.tuning_curve_r = np.vstack((self.tuning_curve_r, np.sum(np.heaviside(i-self.ttx_gone,0)*(da_r - ds_r + nr) * self.mr, axis=1)))
+                   self.tuning_curve_r = np.vstack((self.tuning_curve_r, np.sum(ttx_factor *(da_r - ds_r + nr) * self.mr, axis=1)))
+  
                 # update left weights for inhibitory neuron
-                self.mli = self.mli + (1 / self.tau_w) * self.dt * (
-                            phii * (da_li[k] - ds_li[k] + nli[k]) - self.eps * self.mli)
-                # update right weights for inhibitory neuron
-                self.mri = self.mri + (1 / self.tau_w) * self.dt * (
-                            phii * (da_ri[k] - ds_ri[k] + nri[k]) - self.eps * self.mri)
-
+                self.mli = self.mli + (1/self.tau_w) * self.dt * (phii * (da_li[k] - ds_li[k] + nli[k]) - self.eps*self.mli)  
+                # update right weights for inhibitory neuron      
+                self.mri = self.mri + (1/self.tau_w) * self.dt * (phii * (da_ri[k] - ds_ri[k] + nri[k]) - self.eps*self.mri) 
+                          
                 # Add new weights to the weight matrices
                 self.weights_l = np.vstack((self.weights_l, self.ml))
                 self.weights_r = np.vstack((self.weights_r, self.mr))
                 self.weights_li = np.vstack((self.weights_li, self.mli))
                 self.weights_ri = np.vstack((self.weights_ri, self.mri))
                 # We need to now update theta
-                self.theta = self.theta + (1 / self.tau_theta) * self.dt * (ca ** self.p - self.theta)
-                self.thetai = self.thetai + (1 / self.tau_theta) * self.dt * (ca ** self.p - self.thetai)
+                self.theta = self.theta + (1/self.tau_theta) * self.dt * (ca**self.p-self.theta)
+                self.thetai = self.thetai + (1/self.tau_theta) * self.dt * (ca**self.p-self.thetai)
                 # Add new theta to the vector of thresholds
                 self.thresholds = np.hstack((self.thresholds, self.theta))
                 self.thresholdsi = np.hstack((self.thresholdsi, self.thetai))
                 # Add responses to all orientation in the tuning curve matrix for inhibitory neuron
-                self.tuning_curve_li = np.vstack(
-                    (self.tuning_curve_li, np.sum((da_li - ds_li + nli) * self.mli, axis=1)))
-                self.tuning_curve_ri = np.vstack(
-                    (self.tuning_curve_ri, np.sum((da_ri - ds_ri + nri) * self.mri, axis=1)))
+                self.tuning_curve_li = np.vstack((self.tuning_curve_li, np.sum((da_li - ds_li + nli) * self.mli, axis=1)))
+                self.tuning_curve_ri = np.vstack((self.tuning_curve_ri, np.sum((da_ri - ds_ri + nri) * self.mri, axis=1)))
         # Now delete the first entries (initializations) of the record matrices/vectors
         self.weights_l = np.delete(self.weights_l, 0, axis=0)
         self.weights_r = np.delete(self.weights_r, 0, axis=0)
